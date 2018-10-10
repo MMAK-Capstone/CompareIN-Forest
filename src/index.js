fd0ceb36a6d87b2ebc1c51a42f33d79eed706b03
Megha Patel <meghs2512@gmail.com>
2018-10-10 10:48:53 -0400

    Changes Saved

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
-import constants from "./config/constants";
-import GameScene from "./scenes/game";
+import constants from './config/constants';
+import GameScene from './scenes/game';
 
 const config = {
-  type: Phaser.AUTO,
-  width: constants.WIDTH,
-  height: constants.HEIGHT,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: {
-        y: 200
-      },
-      debug: false
-    }
-  },
-  scene: [GameScene]
+	type: Phaser.AUTO,
+	width: constants.WIDTH,
+	height: constants.HEIGHT,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: {
+				y: 200
+			},
+			debug: false
+		}
+	},
+	scene: [ GameScene ]
 };
 
 // eslint-disable-next-line no-new
 new Phaser.Game(config);
 
 if (module.hot) {
-  module.hot.accept(() => {});
+	module.hot.accept(() => {});
 
-  module.hot.dispose(() => {
-    window.location.reload();
-  });
+	module.hot.dispose(() => {
+		window.location.reload();
+	});
 }
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,83 +1,95 @@
 import Phaser from 'phaser';
+import bg from '../assets/bg.jpg';
+import lessButton from '../assets/lessthan.png';
+import book from '../assets/book.png';
+import banner from '../assets/banner.png';
+import wrongAns from '../assets/wrongAns.png';
+import scoreImg from '../assets/score.png';
+import greaterButton from '../assets/greaterthan.png';
+import equalButton from '../assets/equal.png';
+import airplane from '../assets/airplane.png';
 
-import sky from '../assets/sky.png';
-import ground from '../assets/platform.png';
-import dude from '../assets/dude.png';
+let number1Text;
+let number2Text;
+let headingText;
+let resultText;
+let score = 0;
+let scoreText;
+const Game = new Phaser.Scene('Game');
+Game.preload = function() {
+	this.load.image('bg', bg);
+	this.load.image('book1', book);
+	this.load.image('book2', book);
+	this.load.image('scoreImg', scoreImg);
+	this.load.image('lessButton', lessButton);
+	this.load.image('greaterButton', greaterButton);
+	this.load.image('equalButton', equalButton);
+	this.load.image('airplane', airplane);
+};
 
-class Game extends Phaser.Scene {
-  constructor() {
-    super({ key: 'Game' });
-  }
+Game.create = function() {
+	this.add.image(400, 300, 'bg').setScale(0.5);
+	this.add.image(250, 300, 'book1');
+	this.add.image(500, 300, 'book2');
+	this.add.image(540, 100, 'scoreImg');
 
-  preload() {
-    this.load.image('sky', sky);
-    this.load.image('ground', ground);
-    this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
-  }
+	this.airplaneBanner = this.physics.add.sprite(1100, 200, 'airplane');
+	this.airplaneBanner.body.setAllowGravity(false);
 
-  create() {
-    // add Sky background sprit
-    this.add.image(400, 300, 'sky');
+	let number1 = Math.floor(Math.random() * 100);
+	let number2 = Math.floor(Math.random() * 100);
+	// let heading = 'Compare two numbers';
+	// headingText = this.add.text(30, 30, heading, { fontFamily: 'Amarante', fontSize: '20px', fill: '#000' });
+	number1Text = this.add.text(260, 270, number1, { fontSize: '50px', fill: '#000' });
+	number2Text = this.add.text(520, 270, number2, { fontSize: '50px', fill: '#000' });
+	resultText = this.add.text(300, 400, '', { fontSize: '30px', fill: 'red' });
+	scoreText = this.add.text(470, 60, scoreText, { fontSize: '27px', fill: '#000' });
 
-    // Create ground platforms
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms
-      .create(400, 568, 'ground')
-      .setScale(2)
-      .refreshBody();
-    this.platforms.create(600, 400, 'ground');
-    this.platforms.create(50, 250, 'ground');
-    this.platforms.create(750, 220, 'ground');
+	scoreText.setText('Score: ' + score);
+	const leButton = this.add.image(220, 500, 'lessButton').setInteractive({ useHandCursor: true });
+	leButton.on('pointerdown', () => {
+		if (number1 < number2) {
+			this.result = 'right';
+			score += 5;
+			scoreText.setText('Score:' + score);
+			this.scene.restart();
+		} else if (number1 > number2 || number1 === number2) {
+			// console.log('Wrong Answer...');
+			resultText.setText('Wrong Answer!!!');
+		}
+	});
+	const gtButton = this.add.image(440, 500, 'greaterButton').setInteractive({ useHandCursor: true });
+	gtButton.on('pointerdown', () => {
+		if (number1 > number2) {
+			score += 5;
+			scoreText.setText('Score ' + score);
+			this.scene.restart();
+		} else if (number1 < number2 || number1 === number2) {
+			// console.log('Wrong Answer...');
+			resultText.setText('Wrong Answer!!!');
+		}
+	});
 
-    // Create Player
-    this.player = this.physics.add.sprite(100, 450, 'dude');
-    this.player.body.setGravityY(300);
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
+	const eqButton = this.add.image(660, 500, 'equalButton').setInteractive({ useHandCursor: true });
+	eqButton.on('pointerdown', () => {
+		if (number1 === number2) {
+			score += 5;
+			scoreText.setText('Score ' + score);
+			this.scene.restart();
+		} else if (number1 < number2 || number1 > number2) {
+			// console.log('Wrong Answer...');
+			resultText.setText('Wrong Answer!!!');
+		}
+	});
 
-    // Create player animation
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
+	// headingText.setText(heading);
+	number1Text.setText(number1);
+	number2Text.setText(number2);
+};
 
-    this.anims.create({
-      key: 'turn',
-      frames: [{ key: 'dude', frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    // set collides between Player and grounds
-    this.physics.add.collider(this.player, this.platforms);
-  }
-
-  update() {
-    // Create movement controller
-    this.cursors = this.input.keyboard.createCursorKeys();
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play('left', true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play('right', true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play('turn');
-    }
-
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-450);
-    }
-  }
-}
-
+Game.update = function() {
+	if (score >= 10) {
+		this.airplaneBanner.x -= 4;
+	}
+};
 export default Game;
