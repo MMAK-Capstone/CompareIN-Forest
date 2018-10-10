3861413c0775aa65f57fe3ae097dee99654903b3
Megha Patel <meghs2512@gmail.com>
2018-10-10 16:48:09 -0400

    Final Compare-In-Forest Game

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -9,8 +9,9 @@ import equalButton from '../assets/equal.png';
 import airplane from '../assets/airplane.png';
 import playAgain from '../assets/play-again.png';
 import animal1 from '../assets/animal1.png';
-
+import ruleBoard from '../assets/ruleBoard2.png';
 import flowerEnding from '../assets/flowerEnding.png';
+
 let number1Text;
 let number2Text;
 const WinningText = 'YOU WON!!!';
@@ -32,6 +33,7 @@ Game.preload = function() {
 	this.load.image('playAgain', playAgain);
 	this.load.image('animal1', animal1);
 	this.load.image('flowerEnding', flowerEnding);
+	this.load.image('ruleBoard', ruleBoard);
 };
 
 Game.create = function() {
@@ -39,18 +41,18 @@ Game.create = function() {
 	this.add.image(250, 300, 'book1');
 	this.add.image(500, 300, 'book2');
 	this.add.image(540, 100, 'scoreImg');
+	this.add.image(100, 50, 'ruleBoard').setScale(0.6);
 
 	this.airplaneBanner = this.physics.add.sprite(1100, 200, 'airplane');
 	this.airplaneBanner.body.setAllowGravity(false);
 
 	let number1 = Game.generateNumbers();
 	let number2 = Game.generateNumbers();
-	// let heading = 'Compare two numbers';
-	// headingText = this.add.text(30, 30, heading, { fontFamily: 'Amarante', fontSize: '20px', fill: '#000' });
-	number1Text = this.add.text(260, 270, number1, { fontSize: '50px', fill: '#000' });
-	number2Text = this.add.text(520, 270, number2, { fontSize: '50px', fill: '#000' });
-	resultText = this.add.text(170, 380, '', { fontSize: '60px', fill: 'red' });
-	scoreText = this.add.text(470, 60, scoreText, { fontSize: '27px', fill: '#000' });
+
+	number1Text = this.add.text(260, 270, number1, { fontFamily: 'Chalkduster', fontSize: '50px', fill: '#000' });
+	number2Text = this.add.text(520, 270, number2, { fontFamily: 'Chalkduster', fontSize: '50px', fill: '#000' });
+	resultText = this.add.text(170, 380, '', { fontFamily: 'Chalkduster', fontSize: '50px', fill: 'red' });
+	scoreText = this.add.text(470, 60, scoreText, { fontFamily: 'Chalkduster', fontSize: '27px', fill: '#000' });
 
 	scoreText.setText('Score: ' + score);
 	this.leButton = this.add.image(220, 500, 'lessButton').setInteractive({ useHandCursor: true });
@@ -67,10 +69,8 @@ Game.create = function() {
 				callback: this.restart1,
 				callbackScope: this
 			});
-			// this.scene.restart();
 		}
 	});
-	// this.leButton = this.physics.add.image('lessButton');
 	this.gtButton = this.add.image(440, 500, 'greaterButton').setInteractive({ useHandCursor: true });
 	this.gtButton.on('pointerdown', () => {
 		if (number1 > number2) {
@@ -84,7 +84,6 @@ Game.create = function() {
 				callback: this.restart1,
 				callbackScope: this
 			});
-			// this.scene.restart();
 		}
 	});
 
@@ -111,7 +110,7 @@ Game.restart1 = function() {
 	this.scene.restart();
 };
 Game.update = function() {
-	if (score >= 50) {
+	if (score >= 5) {
 		this.airplaneBanner.x -= 4;
 		if (this.airplaneBanner.x <= -250) {
 			this.scene.start(endingScene);
@@ -132,7 +131,7 @@ endingScene.create = function() {
 	this.add.image(430, 245, 'endingSceneImg').setScale(0.23);
 	this.add.image(270, 240, 'animal1');
 	this.add.image(320, 220, 'flowerEnding');
-	this.add.text(330, 200, WinningText, { fontFamily: 'Amarante', fontSize: '60px', fill: '#000' });
+	this.add.text(330, 200, WinningText, { fontFamily: 'Chalkduster', fontSize: '50px', fill: '#000' });
 	const playAgainButton = this.add
 		.sprite(535, 350, 'playAgain')
 		.setScale(0.5)
